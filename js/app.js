d25810dfe33c038841a54bedb1d2f1a2fc0855d4
zhSHUVO <zhshuvo99@gmail.com>
2022-03-11 19:56:58 +0600

    q&a hidden in liked and report

--- js/app.js
+++ js/app.js
@@ -36,10 +36,12 @@ const displayContent = (text) => {
 
 const switchTab = (id) => {
     if (id === "posts") {
+        document.getElementById("q&a").style.display = "block";
         document.getElementById("posts").style.display = "grid";
         document.getElementById("liked").style.display = "none";
         document.getElementById("reported").style.display = "none";
     } else if (id === "liked") {
+        document.getElementById("q&a").style.display = "none";
         document.getElementById("liked").style.display = "block";
         document.getElementById("liked").innerText = null;
         document.getElementById("posts").style.display = "none";
@@ -47,6 +49,8 @@ const switchTab = (id) => {
 
         displayLikedPosts();
     } else {
+        document.getElementById("q&a").style.display = "none";
+        document.getElementById("reported").innerText = null;
         document.getElementById("reported").style.display = "block";
         document.getElementById("posts").style.display = "none";
         document.getElementById("liked").style.display = "none";
